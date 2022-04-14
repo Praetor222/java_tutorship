f07af84a201066b95ac800bf652a7e56780c5896
Praetor222 <102309131+Praetor222@users.noreply.github.com>
2022-04-14 21:33:51 +0200

    Update Testmain.java

--- Test/src/Testpack/Testmain.java
+++ Test/src/Testpack/Testmain.java
@@ -3,5 +3,6 @@ package Testpack;
 public class Testmain {
 	public static void main (String[] args) {
 		System.out.println("Victor is the best");
+		System.out.println("But everyone know that Marc is a lot better :)");
 	}
 }
